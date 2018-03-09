5ea03e8b86a3064c45f13109fa52777db9acac59
Polery <786705471@qq.com>
2018-03-09 18:23:09 +0800

    aa
    aa

---
+++ xmpp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xmpp
+//
+//  Created by gy on 2018/3/9.
+//  Copyright © 2018年 ***. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ xmpp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  xmpp
+//
+//  Created by gy on 2018/3/9.
+//  Copyright © 2018年 ***. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
